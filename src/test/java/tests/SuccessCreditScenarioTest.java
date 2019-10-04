7d2befc6e74bfd6d7b5a436d9996cb88f16a1538
yash <yash@telekom-digital.com>
2019-10-05 00:10:22 +0530

    Added Failure Scenario

--- src/test/java/tests/SuccessCreditScenarioTest.java
+++ src/test/java/tests/SuccessCreditScenarioTest.java
@@ -53,4 +53,28 @@ public class SuccessCreditScenarioTest extends BaseTestCase {
         creditCardStepsObj.user_click_on_ok_button();;
         //creditCardStepsObj.verify_success_message();
     }
+
+    @Test
+    public void verify_failure_scenario() {
+        String home_page_url = CommonUtils.getBaseUrl();
+        String cardNumber="4911 1111 1111 1113";
+        successCreditScenarioStepsObj.open_home_page(home_page_url);
+        successCreditScenarioStepsObj.user_on_click_buy_now();
+        shoppingCartStepsObj.verify_Shopping_Cart_IsOpened();
+        shoppingCartStepsObj.verify_user_details();
+        shoppingCartStepsObj.click_on_checkout_button();
+        orderSummaryStepsObj.verify_order_summary();
+        orderSummaryStepsObj.click_on_shipping_details();
+        orderSummaryStepsObj.verify_shipping_details();
+        orderSummaryStepsObj.click_on_continue_button();
+        orderSummaryStepsObj.click_on_credit_card();
+        creditCardStepsObj.verify_credit_card_screen_is_displayed();
+        creditCardStepsObj.user_enter_credit_card_number(cardNumber);
+        creditCardStepsObj.user_enter_expiry_date(expiryDate);
+        creditCardStepsObj.user_enter_cvv_number(cvv);
+        creditCardStepsObj.click_on_pay_now();
+        creditCardStepsObj.user_enter_otp(otp);
+        creditCardStepsObj.user_click_on_ok_button();;
+        //creditCardStepsObj.verify_success_message();
+    }
 }
